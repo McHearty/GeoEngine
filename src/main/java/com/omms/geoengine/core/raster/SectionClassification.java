e56bafb23fbef5821cf17248089568bc673cfc6c
Jorden <McHearty@gmail.com>
2026-09-16 19:11:57 +0300

    JavaDoc

--- src/main/java/com/omms/geoengine/core/raster/SectionClassification.java
+++ src/main/java/com/omms/geoengine/core/raster/SectionClassification.java
@@ -1,7 +1,15 @@
 package com.geoengine.core.raster;
 
+/**
+ * Classification of a vertical terrain section for rasterization.
+ */
 public enum SectionClassification {
+    /** Section can be treated as entirely solid. */
     SOLID,
+
+    /** Section can be treated as entirely air. */
     AIR,
+
+    /** Section requires per-block or finer-grained terrain evaluation. */
     BAND
 }
