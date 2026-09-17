f40b50dcde7538d7500045ea137895374e47aa3b
Jorden <McHearty@gmail.com>
2026-09-17 11:44:20 +0300

    JavaDoc

--- src/main/java/com/omms/geoengine/core/material/RockFamily.java
+++ src/main/java/com/omms/geoengine/core/material/RockFamily.java
@@ -1,12 +1,31 @@
 package com.geoengine.core.material;
 
+/**
+ * Material families that can be assigned by the lithology system.
+ */
 public enum RockFamily {
+
+    /** Sedimentary rock formed from sandstone deposits. */
     SEDIMENTARY_SANDSTONE,
+
+    /** Sedimentary limestone used by shallow stratification rules. */
     SEDIMENTARY_LIMESTONE,
+
+    /** Igneous granite assigned by the intrusion field. */
     IGNEOUS_GRANITE,
+
+    /** Igneous diorite assigned by the intrusion field. */
     IGNEOUS_DIORITE,
+
+    /** Igneous andesite assigned by shallow stratification rules. */
     IGNEOUS_ANDESITE,
+
+    /** Metamorphic tuff assigned by a shallow band within the stratification field. */
     METAMORPHIC_TUFF,
+
+    /** Default rock family when no specialized lithology rule matches. */
     STANDARD_STONE,
+
+    /** Deep rock family assigned below the deepslate transition. */
     DEEP_DEEPSLATE
 }
